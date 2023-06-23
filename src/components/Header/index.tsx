356d5feadd778899249b22954c1ebb0b2121871b
Marcos Antonio <marcosantonio-10@live.com>
2023-06-23 15:55:55 -0300

    feture/home: fixe header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import { Cover } from "../../assets";
+import { Cover, GitHubLogo } from "../../assets";
 import { Container } from "./styles";
 
 export function Header() {
   return (
     <Container>
-        <img src={Cover} alt="Header cover" />
+      <img src={GitHubLogo} alt="" />
     </Container>
   )
 }
