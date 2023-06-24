e3555fa3f440d44731647ad4a328f7324738a0d7
Marcos Antonio <marcosantonio-10@live.com>
2023-06-24 13:09:33 -0300

    feature/home: github use r info ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
-import { GithubInfo, Header } from "../../components";
+import { useEffect, useState } from "react";
+import { GithubInfo } from "../../components";
 import { BoxModel } from "../../components/Box";
 import { PageContainer } from "../../components/PageContainer";
 import { PostCard } from "../../components/Post";
 import { PostsContainer, StyledForm } from "./styles";
 import {
   BoxContent,
-  Container,
   ContentInfo,
   GitHubLink,
   GithubInfoContainer,
@@ -17,34 +17,59 @@ import {
   UserName,
 } from "./styles";
 import { ArrowSquareOut } from "@phosphor-icons/react";
+import axios from "axios";
+
+type UserInfo = {
+  name: string;
+  html_url: string;
+  avatar_url: string;
+  bio: string;
+  company?: string;
+  followers?: number;
+  login: string;
+};
 
 export function Home() {
+  const [userData, setUserData] = useState<UserInfo>({} as UserInfo);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/marcoslima12")
+      .then(function (response) {
+        setUserData(response.data);
+        console.log(userData);
+      })
+      .catch(function (error) {
+        // manipula erros da requisição
+        console.error(error);
+      });
+  }, []);
+
   return (
     <PageContainer>
       <BoxModel>
         <BoxContent>
-          <StyledImage
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
-            alt=""
-          />
+          <StyledImage src={userData?.avatar_url} alt="" />
           <ContentInfo>
             <NameGithubContainer>
-              <UserName>Person Name</UserName>
-              <GitHubLink href="#">
+              <UserName>{userData?.name || "Person name"}</UserName>
+              <GitHubLink target="_blank" href={userData?.html_url || "#"}>
                 GITHUB
                 <ArrowSquareOut size={16} />
               </GitHubLink>
             </NameGithubContainer>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              neque, itaque ducimus tenetur excepturi impedit odio alias nisi
-              omnis ipsam dolor explicabo mollitia velit reiciendis officia
-              ullam quos nemo ab.
-            </p>
+            <p>{userData?.bio || "User bio"}</p>
             <GithubInfoContainer>
-              <GithubInfo infoType="username" />
-              <GithubInfo infoType="company" />
-              <GithubInfo infoType="followers_number" />
+              <GithubInfo infoType="username" username={userData?.login} />
+              {userData?.company && (
+                <GithubInfo infoType="company" company={userData?.company} />
+              )}
+              {userData?.followers && (
+                <GithubInfo
+                  infoType="followers_number"
+                  followers_number={userData?.followers}
+                />
+              )}
             </GithubInfoContainer>
           </ContentInfo>
         </BoxContent>
